1164d0eacb1283aeb8d64a67f768858ce13d5b65
Mohammed Noori <kmanoorib@gmail.com>
2018-03-04 21:17:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendKitten(name){
   return kittens.push(name);
 }
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name);
+}
